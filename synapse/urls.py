20515e6f4610aa7ffd46efb55f4ac005bf982812
IMperiumX <yusufadell.dev@gmail.com>
2024-12-28 13:44:48 +0200

    Add URL patterns for API endpoints and static file serving

--- synapse/urls.py
+++ synapse/urls.py
@@ -15,11 +15,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.urls.conf import include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tinymce/", include("tinymce.urls")),
+]
+
+if settings.DEBUG:
+    # Static file serving when using Gunicorn + Uvicorn for local web socket>
+    urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += [
+    # API base url
+    path("api/", include("synapse.api_router")),
+    # DRF auth token
+    path(
+        "api-auth/", include("rest_framework.urls", namespace="rest_framework")
+    ),  # include login URLs for the browsable API.
+    path("api/auth-token/", obtain_auth_token),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
+    path("", TemplateView.as_view(template_name="pages/home.html")),
 ]
